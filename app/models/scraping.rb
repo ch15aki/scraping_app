ee09c25e09d2c8f74b5b06a3378dbdcd09da83b5
ch15aki <and.1la.view@gmail.com>
2020-08-27 21:49:39 +0900

    全ページから情報取得する実装

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,12 +2,23 @@ class Scraping
   def self.nux_urls
     links = []
     agent = Mechanize.new
-    current_page = agent.get("https://nuxactive.com/collections/shop-all")
-    elements = current_page.search('.grid-product__content a')
 
-    elements.each do |ele|
-      links << ele.get_attribute('href')
+    next_url = "/collections/shop-all"
+
+    while true do 
+      current_page = agent.get("https://nuxactive.com" + next_url)
+      elements = current_page.search('.grid-product__content a')
+  
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.pagination .next a')
+      break unless  next_link
+
+      next_url = next_link[:href]
     end
+
 
     links.each do |link|
       get_nux("https://nuxactive.com" + link)
@@ -21,7 +32,9 @@ class Scraping
   price = page.at('.product__price').inner_text
   url = page.at('.MagicToolboxSlide img')[:src] if page.at('.MagicToolboxContainer img')
   image_url = "https:#{url}"
-  product = Product.where(name: name, price: price, image_url: image_url).first_or_initialize
+  product = Product.where(name: name).first_or_initialize
+  product.price = price
+  product.image_url = image_url
   product.save
   end
 end
